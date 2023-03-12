45e84347ca6e5d32f960b51eb974df00686b8850
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-12 15:27:32 -0400

    Add create resolvers for Game and User

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Game } = require('../models');    // will eventually import Order as well
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');    // will use this if we get to Stripe
 
 const resolvers = {
   Query: {
@@ -25,12 +25,42 @@ const resolvers = {
     // ***nice to have**  get all users (sellers -- a new page, where they can sort by rating),
   },
   Mutation: {
+    // CREATE one user -- currnetly verbatim for example
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
 
       return { token, user };
     },
+    // CREATE one game (will need a form for this -- also updates the seller's games array)
+    addGame: async (parent, args, context) => {
+      if (context.user) {
+        const game = await Game.create({
+          ...args, 
+          seller: context.user._id});
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { games: game._id }}
+        );
+
+        return game;
+      }
+      throw new AuthenticationError('Only sellers can list games; login or sign-up first!');
+    },
+    
+// mutations
+
+    // UPDATE user (would use thes signup form logic)
+    // DELETE one user (i.e. delete account -- should also 'cascade' delete the games in the assoc. array)
+
+
+    // UPDATE one game (probably just price -- probly can use the same form as CREATE game)
+    // DELETE one game (remember to remove it from User's games array)
+
+    // CREATE ORDER (from cart, which holds array of games (id) to buy -- once order submitted, THEN create)
+
+
     addOrder: async (parent, { products }, context) => {
       console.log(context);
       if (context.user) {
@@ -74,18 +104,5 @@ const resolvers = {
     }
   }
 };
-
-
-
-// mutations
-    // CREATE one user (similar to sign up)
-    // UPDATE user (would use thes signup form logic)
-    // DELETE one user (i.e. delete account -- should also 'cascade' delete the games in the assoc. array)
-
-    // CREATE one game (will need a form for this)
-    // UPDATE one game (probably just price -- probly can use the same form as CREATE game)
-    // DELETE one game (remember to remove it from User's games array)
-
-    // CREATE ORDER (from cart, which holds array of games (id) to buy -- once order submitted, THEN create)
 
 module.exports = resolvers;

d71d2c3ac72a69358093dd52b8be5540f6729637
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-12 15:12:20 -0400

    Add get-one seller w/ assoc. games, and plans for a couple more

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,93 +17,12 @@ const resolvers = {
     game: async (parent, { gameId }) => {
       return await Game.findById(gameId).populate('seller');
     },
-
-    // get one user (also grab their games array)
-    // ***nice to have**      // get all users (sellers -- a new page, where they can sort by rating)
-
-    // get all orders (connected to one user)
-
-
-    products: async (parent, { category, name }) => {
-      const params = {};
-
-      if (category) {
-        params.category = category;
-      }
-
-      if (name) {
-        params.name = {
-          $regex: name
-        };
-      }
-
-      return await Product.find(params).populate('category');
+    // get ONE seller (User) by ID
+    seller: async (parent, { userId }) => {
+      return await User.findById(userId).populate('games');
     },
-    product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
-    },
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
-
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
-        return user;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    order: async (parent, { _id }, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
-
-        return user.orders.id(_id);
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ products: args.products });
-      const line_items = [];
-
-      const { products } = await order.populate('products');
-
-      for (let i = 0; i < products.length; i++) {
-        const product = await stripe.products.create({
-          name: products[i].name,
-          description: products[i].description,
-          images: [`${url}/images/${products[i].image}`]
-        });
-
-        const price = await stripe.prices.create({
-          product: product.id,
-          unit_amount: products[i].price * 100,
-          currency: 'usd',
-        });
-
-        line_items.push({
-          price: price.id,
-          quantity: 1
-        });
-      }
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`
-      });
-
-      return { session: session.id };
-    }
+    // GET all orders (by one user, buyer OR seller -- so this may need to be 2 separate routes),
+    // ***nice to have**  get all users (sellers -- a new page, where they can sort by rating),
   },
   Mutation: {
     addUser: async (parent, args) => {

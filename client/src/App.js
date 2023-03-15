06cdbaed3ebb568ae6aad0b4ae60431f664c9943
Noah Cote <noahcote10@gmail.com>
2023-03-14 20:33:23 -0400

    fixed paths and code to correctly render container and its contents

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@
 import React, { useState, useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div>
       <Header />
+      <Container />
       <Footer />
     </div>
   );

224635588e912dd0ac02a45630481656eef3bdf9
Tyler Bedard <tylerbedard126@gmail.com>
2023-03-15 20:57:42 -0400

    bootstrap working

--- client/src/pages/Home/components/Game/index.js
+++ client/src/pages/Home/components/Game/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardGroup } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
 import { useQuery } from "@apollo/client";
 import { QUERY_ALL_GAMES } from "../../../../graphql/queries";
 
@@ -7,21 +7,25 @@ export default function GameCard() {
   const { loading, error, data } = useQuery(QUERY_ALL_GAMES);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>You fucked something up</p>;
+  if (error) return <p>Error</p>;
 
   return (
     <div>
-      <h1>Video Games</h1>
-      <CardGroup>
+      <h1>Games for sale!</h1>
+      <Row xs={1} sm={2} md={3} className="g-4">
         {data.games.map((games) => (
-          <Card key={games._id} style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={games.imgUrl} />
-            <Card.Body>
-              <Card.Title>{games.imgUrl}</Card.Title>
-            </Card.Body>
-          </Card>
+          <Col key={games._id}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={games.imgUrl} />
+              <Card.Body className="cardbody">
+                <Card.Title>{games.name}</Card.Title>
+                <Card.Text>{games.price}</Card.Text>
+                <Card.Text>{games.category}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </CardGroup>
+      </Row>
     </div>
   );
 }

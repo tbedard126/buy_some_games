cb816bdb2fed9bac3bcd225751981f5aac98dc11
Tyler Bedard <tylerbedard126@gmail.com>
2023-03-16 21:55:26 -0400

    some style updates

---
+++ client/src/pages/Seller/Seller.js
@@ -0,0 +1,16 @@
+// import React, { useState, useEffect } from "react";
+// import { useQuery } from "@apollo/client";
+// // import { QUERY_GAME } from "../../../../graphql/queries";
+
+// export default function Seller() {
+//   const { loading, error, data } = useQuery(QUERY_GAME);
+
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error</p>;
+
+//   return (
+//     <div>
+//       <p>This is the Seller page!</p>
+//     </div>
+//   );
+// }

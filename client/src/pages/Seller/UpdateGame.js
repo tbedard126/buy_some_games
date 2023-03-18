9819f3f30b89b9d08f3aafb2f82abcb1068ed847
Noah Cote <noahcote10@gmail.com>
2023-03-18 17:27:28 -0400

    added some functions for UpdateGame and the removeGame button. both need attentionas they don't work.

---
+++ client/src/pages/Seller/UpdateGame.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { UPDATE_GAME } from "../../graphql/mutations";
+import Auth from "../../auth/auth";
+
+export default function UpdateGame({ gameId }) {
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = () => setShowModal(false);
+
+  const [updateGame] = useMutation(UPDATE_GAME);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const description = event.target.description.value;
+    const price = parseFloat(event.target.price.value);
+    const category = event.target.category.value;
+    const { data } = updateGame({
+      variables: {
+        id: gameId,
+        name: name,
+        description: description,
+        price: price,
+        category: category,
+        seller: Auth.getProfile().data._id,
+      },
+    })
+      .then(() => {
+        console.log(data);
+        handleClose();
+      })
+      .catch((error) => {
+        console.log(data);
+        console.error(error);
+      });
+  };
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setShowModal(true)}>
+        Update Game
+      </Button>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Game</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter game name" />
+            </Form.Group>
+
+            <Form.Group controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Enter game description"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                step="0.01"
+                placeholder="Enter game price"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="category">
+              <Form.Label>Category</Form.Label>
+              <Form.Control as="select">
+                <option>Super Nintendo</option>
+                <option>Nintendo 64</option>
+                <option>Sega Genesis</option>
+                <option>Nintendo</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Update Game
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+

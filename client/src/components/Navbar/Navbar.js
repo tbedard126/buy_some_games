156a1a5e925f62395ca3b66032e81858c0d28c30
Noah Cote <noahcote10@gmail.com>
2023-03-19 00:01:09 -0400

    added styling to navbar so each link is not mismatched

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,44 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { Nav } from "react-bootstrap";
-import { Link } from 'react-router-dom';
-import Auth from '../../auth/auth';
+import { Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Auth from "../../auth/auth";
 import AddGame from "../addGame";
 
 export default function Navbar({ currentPage, handlePageChange }) {
-
   const handleLogout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
 
   return (
-    <Nav variant="tabs">
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      {Auth.loggedIn() ? (
-        <>
-          <span>Signed in as {Auth.getProfile().data.username}</span>
-          <Link className="btn btn-lg btn-info m-2" to={`/users/${Auth.getProfile().data._id}`}>
-            {Auth.getProfile().data.username}'s profile
-          </Link>
-          <AddGame />
-          <button className="btn btn-lg btn-light m-2" onClick={handleLogout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <Link className="btn" to="/login">
-            Login
-          </Link>
-          <Link className="btn" to="/signup">
-            Signup
-          </Link>
-        </>
-      )}
-      <div>
-        <button>Cart</button>
+    <Nav>
+      <div className="navbar">
+        <Link to="/" className="nav-btn">
+          Home
+        </Link>
+        <div className="user-buttons">
+          {Auth.loggedIn() ? (
+            <>
+              <Link
+                className="nav-btn m-2"
+                to={`/users/${Auth.getProfile().data._id}`}
+              >
+                {Auth.getProfile().data.username}'s profile
+              </Link>
+              <AddGame />
+              <span className="nav-btn m-2" onClick={handleLogout}>
+                Logout
+              </span>
+            </>
+          ) : (
+            <>
+              <Link className="nav-btn m-2" to="/login">
+                Login
+              </Link>
+              <Link className="nav-btn m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+        <span className="nav-btn m-2 float-right">Cart</span>
       </div>
     </Nav>
   );

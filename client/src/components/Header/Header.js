49d38934ed6ab0d65a7e48d50fa1701e821ae40f
Noah Cote <noahcote10@gmail.com>
2023-03-19 18:44:05 -0400

    tweaked cards on homepage and seller page. centered titles as well.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,8 +3,8 @@ import { Container } from "react-bootstrap";
 
 export default function Header() {
   return (
-    <Container fluid className="header">
-      <h1>Buy Some Games<img className="controller"src="./images/controller-icon.png" alt="controller"></img></h1>
+    <Container fluid className="headerCont">
+      <h1 className="header">Buy Some Games<img className="controller"src="./images/controller-icon.png" alt="controller"></img></h1>
     </Container>
   );
 }

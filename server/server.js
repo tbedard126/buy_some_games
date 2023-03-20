9e608e44619065a505d32318c57b750b153e71d1
Tyler Bedard <tylerbedard126@gmail.com>
2023-03-19 21:15:30 -0400

    *

--- server/server.js
+++ server/server.js
@@ -23,13 +23,9 @@ app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
 }
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
